ede839e1db0c12aade0bea052864e869d8f1fd02
Shri10 <pethe.shriraj@gmail.com>
2023-06-25 20:56:11 +0530

    [Shri10] first commit

---
+++ DS_StacksQueues/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DS_StacksQueues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
